2ddfeb4bd15860157bcaeab985a41dd81912490a
Reeves, Trevor <trevor.reeves@dnv.com>
2026-09-17 19:40:11 +0100

    fixing versioning strategy for first PRs on release branch

--- rehearsals/release-flow.ps1
+++ rehearsals/release-flow.ps1
@@ -31,6 +31,11 @@ function Invoke-InitReleaseBranch {
     Set-GitBranch main
     New-GitBranch "release/$version/main"
 
+    # Perform an empty commit to establish the root of the release branch
+    # This commit ensures that pull requests branches at the start of the release branch
+    # recieve the correct version; it allows GitVersion to correctly determine the correct parent branch.
+    Add-GitCommit ./info.txt -Message "Release-Flow: Initialize release branch : release/$version/main"
+
     # To ensure that any commits on a 'fix' branch, for the first release on a release branch, get the correct
     # version number generated, this tag is required at the root of the release branch. 
     # Otherwise the major.minor.path is not right; this is because there is no reliable way for GitVersion to determine
